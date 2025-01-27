768577650d71d5cb7b0dec425cb16f8be77ca743
Takhir Latypov <cregennandev@gmail.com>
2025-01-27 12:22:02 +0300

    Тесты работают

--- test/YdbDriver.test.ts
+++ test/YdbDriver.test.ts
@@ -9,26 +9,35 @@ describe('YdbDriver', () => {
     let driver: YdbDriver;
 
     beforeAll(async () => {
+        process.env.YDB_ANONYMOUS_CREDENTIALS = '1';
+        process.env.TESTCONTAINERS_HOST_OVERRIDE="127.0.0.1";
+
         const env = await YdbRunner.getTestingEnvironment();
+
         container = env.container;
         environment = env.composeEnvironment;
         driver = new YdbDriver({
             database: "local",
             endpoint: `grpc://${container.getHost()}:${container.getMappedPort(2136)}`
         });
-        await driver.query("SELECT 1;");
+        await driver.ready();
+        await driver.testConnection();
     });
 
-    afterAll(async() => {
-        await container.stop();
-        await environment.stop();
+    afterAll(async () => {
+        if (driver)
+            await driver.release();
+        if (container)
+            await container.stop();
+        if (environment)
+            await environment.stop();
     });
 
     test('alive', async() => {
         let data = await driver.query("SELECT 1 as test");
         expect(data).toEqual([
             {
-                name: 1
+                test: "1"
             }
         ]);
     });
--- test/runner.ts
+++ test/runner.ts
@@ -1,21 +1,10 @@
 import {
-    DbRunnerAbstract,
     DBRunnerContainerOptions
 } from "@cubejs-backend/testing-shared/dist/src/db-container-runners/db-runner.abstract";
 import {
-    BoundPorts,
     DockerComposeEnvironment,
-    GenericContainer,
-    StartedDockerComposeEnvironment,
     Wait,
-    WaitStrategy
 } from "testcontainers";
-import {StartedGenericContainer} from "testcontainers/build/generic-container/started-generic-container";
-
-export type YdbDbRunnerContainerOptions = {
-    grpcPort?: string;
-    grpcTlsPort?: string;
-} & DBRunnerContainerOptions;
 
 export class YdbRunner{
     public static async getTestingEnvironment(){
